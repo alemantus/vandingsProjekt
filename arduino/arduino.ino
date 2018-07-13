172584dc20ed6e604c046dd36f2db7c1a169c668
alemantus <Alexander@anak.dk>
2018-07-13 18:04:48 +0200

    working

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,42 +1,79 @@
 // **** INCLUDES *****
 #include "LowPower.h"
+#include <avr/sleep.h>
 
 // Use pin 2 as wake up pin
-const int wakeUpPin = 2;
-int data[2];
-void wakeUp()
+long int placeHolder = 0;
+long int data[2];
+
+void sleepNow()         // here we put the arduino to sleep
 {
-    // Just a handler for the pin interrupt.
+
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
+
+  sleep_enable();          // enables the sleep bit in the mcucr register
+  // so sleep is possible. just a safety pin
+
+
+  attachInterrupt(0, wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function
+  // wakeUpNow when pin 2 gets LOW
+
+  sleep_mode();            // here the device is actually put to sleep!!
+  // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
+
+  sleep_disable();         // first thing after waking from sleep:
+  // disable sleep...
+  detachInterrupt(0);      // disables interrupt 0 on pin 2 so the
+  // wakeUpNow code will not be executed
+  // during normal running time.
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(500);
+
 }
 
+void wakeUpNow(void)
+{
+ 
+}
 void setup()
 {
-    // Configure wake up pin as input.
-    // This will consumes few uA of current.
-    pinMode(wakeUpPin, INPUT);   
-    Serial.begin(115200);
+  // Configure wake up pin as input.
+  // This will consumes few uA of current.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(2, INPUT);
+  //pinMode(2,INPUT);
+  Serial.begin(115200);
+  //attachInterrupt(0, pinInterrupt, LOW);
 }
 
-void loop() 
+
+
+void loop()
 {
-    // Allow wake up pin to trigger interrupt on low.
-    attachInterrupt(0, wakeUp, HIGH);
-    
-    // Enter power down state with ADC and BOD module disabled.
-    // Wake up when wake up pin is HIGH.
-    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
-    
-    // Disable external pin interrupt on wake up pin.
-    detachInterrupt(0); 
-
-    
-    //data[0] = analogRead(A0);
-    //data[1] = analogRead(A1);
-
-    data[0] = 11;
-    data[1] = 15;
+  digitalWrite(LED_BUILTIN, LOW);  // turn LED on to indicate awake
+  int i = 0;
+  while(i<5){
+    placeHolder = placeHolder + analogRead(A0);
+    i++;
     delay(200);
-    Serial.println(data[0]);
-    delay(10);
-    Serial.println(data[1]);
+  }
+  
+  data[0] = placeHolder/6;
+  //data[1] = analogRead(A1);
+  placeHolder=0;
+  data[1]=analogRead(A0);
+  //delay(200);
+  //itoa(data[0], str, 10);
+  //Serial.write(String(data[0]).c_str());
+  Serial.write(String(data[1]).c_str());
+  //delay(10);
+  //Serial.println(data[1]);
+  delay(6000);
+  sleepNow();
+
+
+
 }
+
+
--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,21 +1,21 @@
 /*
- Basic ESP8266 MQTT example
+  Basic ESP8266 MQTT example
 
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
+  This sketch demonstrates the capabilities of the pubsub library in combination
+  with the ESP8266 board/library.
 
- It connects to an MQTT server then:
+  It connects to an MQTT server then:
   - publishes "hello world" to the topic "outTopic" every two seconds
   - subscribes to the topic "inTopic", printing out any messages
     it receives. NB - it assumes the received payloads are strings not binary
   - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
     else switch it off
 
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
+  It will reconnect to the server if the connection is lost using a blocking
+  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+  achieve the same result without blocking the main loop.
 
- To install the ESP8266 board, (using Arduino 1.6.4+):
+  To install the ESP8266 board, (using Arduino 1.6.4+):
   - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
        http://arduino.esp8266.com/stable/package_esp8266com_index.json
   - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
@@ -28,53 +28,64 @@
 
 // Update these with values suitable for your network.
 
-const char* ssid = "alemantus";
-const char* password = "Alex1240";
-const char* mqtt_server = "10.0.0.26";
+const char* ssid = "Kvinderegensen";
+const char* password = "qralumne";
+const char* mqtt_server = "87.63.168.126";
 
 const char* serverUser = "admin";
 const char* serverPassword = "Strandparken57";
 
 const char* dataOut[2];
 
+
+
+String inputString = "";         // a String to hold incoming data
+boolean stringComplete = false;
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
+
 char msg[50];
 int value = 0;
 int wakePin = 2;
-String data[2];
+String data[4];
 int sensorNumber = 2;
+char input;
 int error = 0;
+String content = "";
+
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  //  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   pinMode(wakePin, OUTPUT);
   Serial.begin(115200);
-
-  digitalWrite(wakePin, HIGH);
-  delay(500); //give the slave some time to wake up
-  if(error < 5){
-    if (Serial.available() > 0) {
-      for(int i = 0; i < sensorNumber; i++){
-        while(Serial.read()!='\n'){
-          data[i]=Serial.read();
-        }
-      }
-      digitalWrite(wakePin, LOW); 
-    }else{
-      delay(100);
-      error++;
-    }
-  }else{
-    Serial.println("Could wake slave");
-  }
-  data[0] = "1";
-  data[1] = "2";
-  for(int i = 0; i < sensorNumber; i++){ //convert data from int to const char*
-    dataOut[i] = data[i].c_str();    
-  }
   
+  digitalWrite(wakePin, HIGH);
+  delay(200);
+  while (!(Serial.available())) {
+    if (Serial.available()) {
+      break;
+    }
+  }
+  while (Serial.available()) {
+    delay(100);
+    input = Serial.read();
+    content.concat(input);
+    
+  }
+
+  //data[0] = "1";
+  //data[1] = "2";
+  /*
+    for(int i = 0; i < sensorNumber; i++){ //convert data from int to const char*
+    dataOut[i] = data[i].c_str();
+    }
+  */
+  for (int i = 0; i < sensorNumber; i++) { //convert data from int to const char*
+    dataOut[i] = content.c_str();
+  }
+  //dataOut[1] = String(input).c_str();
+  digitalWrite(wakePin, LOW);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
@@ -82,15 +93,20 @@ void setup() {
   if (!client.connected()) {
     reconnect();
   }
+
   client.loop();
-  for(int i = 0; i < sensorNumber; i++){
+
+  for (int i = 0; i < sensorNumber; i++) {
     snprintf (msg, 75, "data%ld", i);
     client.publish(msg, dataOut[i]);
   }
   snprintf (msg, 75, "hello world #%ld", value);
   Serial.println(msg);
+
+
+
   client.publish("outTopic", msg);
-  ESP.deepSleep(600 * 1000000); 
+  ESP.deepSleep(3600 * 1000000);
 }
 
 void setup_wifi() {
@@ -158,3 +174,4 @@ void loop() {
 
 
 }
+
